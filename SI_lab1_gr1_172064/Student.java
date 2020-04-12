5509d96d01b8e9a1083a6e29a8ec618a2c1b911c
velickovicaleksandar <aleksandar2398@gmail.com>
2020-04-12 21:04:26 +0200

    lab   points   variable   added

---
+++ SI_lab1_gr1_172064/Student.java
@@ -0,0 +1,18 @@
+class Student {
+	String index;
+	String firstName;
+	String lastName;
+int labPoints [];
+	//TODO constructor
+
+	//TODO seters & getters
+
+	public double getAverage() {
+		//TODO
+
+
+	public boolean hasSignature() {
+		//TODO
+
+	}
+}
