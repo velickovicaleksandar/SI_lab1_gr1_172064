62ac8aba3cc2110bb4b3c4b534c4a4a5f6becc8b
velickovicaleksandar <aleksandar2398@gmail.com>
2020-04-12 21:11:58 +0200

    student class finished

--- SI_lab1_gr1_172064/Student.java
+++ SI_lab1_gr1_172064/Student.java
@@ -4,15 +4,22 @@ class Student {
 	String lastName;
 int labPoints [];
 	//TODO constructor
-
+Student(){ }
+String getIndex{return index;}
 	//TODO seters & getters
 
 	public double getAverage() {
 		//TODO
-
-
+		int total=0
+for(int  i= 0;  i<labPoints.length ; i++) {
+  total+=labPoints.length;
+}
+	}
 	public boolean hasSignature() {
 		//TODO
+		if(labPoints.length>8){
+			return true;
+		}
 
 	}
 }
